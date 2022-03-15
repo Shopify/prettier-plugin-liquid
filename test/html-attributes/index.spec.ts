9ce069d287166c1ab319ab66a08e9939c7f17cf9
Charles-P. Clermont <charles@cpclermont.com>
2022-03-15 15:54:37 -0400

    Print actual to file when unit test fails. For easier full diff.

--- test/html-attributes/index.spec.ts
+++ test/html-attributes/index.spec.ts
@@ -1,10 +1,8 @@
-import { expectFormatted, readFile } from '../test-helpers';
+import { assertFormattedEqualsFixed } from '../test-helpers';
 import * as path from 'path';
 
 describe(`Unit: ${path.basename(__dirname)}`, () => {
   it('should format as expected', () => {
-    expectFormatted(__dirname, 'index.liquid').to.eql(
-      readFile(__dirname, 'fixed.liquid'),
-    );
+    assertFormattedEqualsFixed(__dirname);
   });
 });
--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -4,18 +4,24 @@ import * as fs from 'fs';
 import * as prettier from 'prettier';
 import * as plugin from '../src';
 
-export function expectFormatted(
-  dirname: string,
-  filename: string,
-  options = {},
-) {
-  const source = readFile(dirname, filename);
+export function assertFormattedEqualsFixed(dirname: string, options = {}) {
+  const source = readFile(dirname, 'index.liquid');
   const formatted = format(source, options);
-  return expect(formatted);
+  const expected = readFile(dirname, 'fixed.liquid');
+  if (expected !== formatted) {
+    writeFile(dirname, 'actual.liquid', expected);
+  } else {
+    fs.rmSync(path.join(dirname, 'actual.liquid'), { force: true });
+  }
+  return expect(formatted).to.eql(expected);
 }
 
 export function readFile(dirname: string, filename: string) {
   return fs.readFileSync(path.join(dirname, filename), 'utf8');
+}
+
+export function writeFile(dirname: string, filename: string, contents: string) {
+  return fs.writeFileSync(path.join(dirname, filename), contents, 'utf8');
 }
 
 export function format(content: string, options: any) {
