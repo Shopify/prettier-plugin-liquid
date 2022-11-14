af62017ae6a6e3b87b0faa0456e1234311c5a87e
Charles-P. Clermont <charles@cpclermont.com>
2022-11-14 10:29:09 -0500

    Add support for `stylesheet` tag
    Fixes #116

--- src/printer/print/liquid.ts
+++ src/printer/print/liquid.ts
@@ -469,6 +469,7 @@ export function printLiquidRawTag(
         ' ',
         node.name,
         ' ',
+        node.markup ? `${node.markup} ` : '',
         node.whitespaceEnd,
         '%}',
       ]);
